1ab24ac625b339b634fe228328e92ec05c1abddf
Eric Pedley <ericpedley@gmail.com>
2021-02-20 14:37:23 -0800

    added movie screen with rendertexture

---
+++ Assets/Scripts/VideoCapture.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoCapture : MonoBehaviour
+{
+    private Camera cam;
+    private Texture2D tex;
+    private RenderTexture rt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        cam.targetTexture = rt;
+        RenderTexture.active=rt;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.Render();
+        tex.ReadPixels(new Rect(0,0,100,100),0,0);
+        Color[] framebuffer = tex.GetPixels();
+    }
+}
