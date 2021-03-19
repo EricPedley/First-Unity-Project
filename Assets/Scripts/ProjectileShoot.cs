a35f74a0651c4b6f8dc207180f0e9bf8a750b7c8
Eric Pedley <ericpedley@gmail.com>
2021-03-19 15:53:37 -0700

    added visual debugging stuff and got prediction code working

--- Assets/Scripts/ProjectileShoot.cs
+++ Assets/Scripts/ProjectileShoot.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class ProjectileShoot : MonoBehaviour
 {
     public GameObject pfBullet;
+    public GameObject pfMarker;
     public Transform predIndicator;
     public Transform fireRef;
     private Rigidbody trackedBullet;
@@ -14,11 +15,6 @@ public class ProjectileShoot : MonoBehaviour
     void Start()
     {
         posHistory = new LinkedList<Vector3>();
-        posHistory.AddFirst(new Vector3(1,0,2));
-        posHistory.AddFirst(new Vector3(2,0,3.8f));
-        posHistory.AddFirst(new Vector3(2.9f,0,6.1f));
-        Vector2 ab = linReg(posHistory);
-        Debug.Log($"a: {ab.x}, b: {ab.y}");
     }
 
     // Update is called once per frame
@@ -34,16 +30,20 @@ public class ProjectileShoot : MonoBehaviour
                     posHistory.RemoveFirst();
                 }
                 posHistory.AddLast(trackedBullet.position);
+                GameObject trailMarker = Instantiate(pfMarker,trackedBullet.position,trackedBullet.rotation);
+                Destroy(trailMarker,1f);
             } else {
                 posHistory.Clear();
             }
-            Debug.Log(posHistory.Count);
             deltaCount=0;
         }
         float elevation = -0.5f;
-        Vector2 predictedPosition = predictPosition(elevation);
-        if(!float.IsNaN(predictedPosition.magnitude))
-            predIndicator.position=new Vector3(predictedPosition.x,elevation,predictedPosition.y);
+        if(posHistory.Count>0) {
+            Vector2 predictedPosition = predictPosition(elevation);
+            if(!float.IsNaN(predictedPosition.magnitude))
+                predIndicator.position=new Vector3(predictedPosition.x,elevation,predictedPosition.y);
+        }
+        
     }
 
     private void shootBullet() {
@@ -51,12 +51,16 @@ public class ProjectileShoot : MonoBehaviour
         trackedBullet = newBullet.GetComponent<Rigidbody>();
         posHistory.Clear();
         trackedBullet.velocity = newBullet.transform.rotation*Vector3.forward*10+new Vector3(Random.value,Random.value,Random.value);
+        Destroy(newBullet,5f);
     }
-    //returns the x,z coordinates where y=elevation for the parabola y=f(x) and xz direction, or y=f(z) if useZ is true
-    private Vector2 predictPosition(float elevation=0,bool useZ=false) {
-        Vector2 direction = linReg(posHistory);
-        Vector3 parabola = quadReg(posHistory,direction.y>direction.x);
-        //solve for zeros of parabola and then compute z coord.
+    //returns the x,z coordinates where y=elevation for the parabola y=f(x), or y=f(z) if useZ is true, and linear relationship between x and z
+    //uses a quadratic equation to model the flight, which is accurate enough for objects whose drag decceleration isn't too large
+    private Vector2 predictPosition(float elevation=0) {
+        Vector2 direction = linReg(posHistory);//direction.x is the slope and direction.y is the y-intercept
+        Debug.DrawLine(new Vector3(0,1,direction.y),new Vector3(1,1,direction.y+direction.x),new Color(255,0,0));
+        bool useZ = direction.x>1;//if the slope is >1, then the change in z is greater so the regression will be better(I think, just on intuition)
+        Vector3 parabola = quadReg(posHistory,useZ);
+        //solve for zeros of parabola and then compute the remaining horizontal coordinate(either x or z)
         float a = parabola.x;
         float b = parabola.y;
         float c = parabola.z+elevation;
@@ -64,17 +68,16 @@ public class ProjectileShoot : MonoBehaviour
         float zero2 = (-b-Mathf.Sqrt(b*b-4*a*c))/(2*a);
         float zero;
         if(useZ){
-            zero = Mathf.Abs(zero1-posHistory.Last.Value.z)<Mathf.Abs(zero2-posHistory.Last.Value.z) ? zero1:zero2;//chooses the zero closest to the last recorded position of the projectile
-            return new Vector2(zero,zero*direction.x/direction.y);//x and z coordinates of where the projectile lands
+            zero = Mathf.Abs(zero1-transform.position.z)>Mathf.Abs(zero2-transform.position.z) ? zero1:zero2;//chooses the zero furthest from the launch point using the z coords
+            return new Vector2((zero-direction.y)/direction.x,zero);//x and z coordinates of where the projectile lands
         } else {
-            //BUG nullReferenceException on following line. Maybe Value.x because it's inserting null vectors in Update?
-            zero = Mathf.Abs(zero1-posHistory.Last.Value.x)<Mathf.Abs(zero2-posHistory.Last.Value.x) ? zero1:zero2;//chooses the zero closest to the last recorded position of the projectile
-            return new Vector2(zero,zero*direction.y/direction.x);//x and z coordinates of where the projectile lands
+            zero = Mathf.Abs(zero1-transform.position.x)>Mathf.Abs(zero2-transform.position.x) ? zero1:zero2;//chooses the zero furthest from the launch point using the x coords
+            return new Vector2(zero,zero*direction.x+direction.y);//x and z coordinates of where the projectile lands
         }
     }
 
     private Vector2 linReg(LinkedList<Vector3> points) {//computes the regression between x and z, where z=ax+b
-        //naive algorithm for computing mean and sample variance. TODO: figure out something like Welford's Algorithm(https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance) except it has to subtract from the agregate was well, or maybe just don't subtract and
+        //naive algorithm for computing mean and sample variance (it sucks because there are two loops). TODO: figure out something like Welford's Algorithm(https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance) except it has to subtract from the agregate was well, or maybe just don't subtract and
         float sumX=0,sumZ=0,difSqSumX=0,difSqSumZ=0;
         foreach(Vector3 p in points) {
             sumX+=p.x;
