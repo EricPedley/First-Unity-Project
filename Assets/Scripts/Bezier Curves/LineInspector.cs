538115bade8b6af561ee6e9ddfa9f5c696bc7f2d
Eric Pedley <ericpedley@gmail.com>
2021-03-19 15:53:19 -0700

    following bezier curve tutorial

---
+++ Assets/Scripts/Bezier Curves/LineInspector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Line))]
+public class LineInspector : Editor
+{
+    private void OnSceneGUI () {
+		Line line = target as Line;
+        Transform trans = line.transform;
+        Quaternion rotation = trans.rotation;
+        Vector3 p0 = trans.TransformPoint(line.p0);
+        Vector3 p1 = trans.TransformPoint(line.p1);
+		Handles.color = Color.white;
+		Handles.DrawLine(p0,p1);
+	}
+}
